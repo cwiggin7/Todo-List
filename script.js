5dcd1e32d2a3b90249c73182f9e50ee071020ba5
cwiggin7 <cwiggin7@ksu.edu>
2023-05-27 23:17:33 -0500

    Functions to create a new project

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// creates a 'todos' object which belongs to a project
-class todos {
+const project_form = document.getElementById('project-form');
+const project_form_input = document.getElementById('project-form-input');
+
+//  a class representing a todo item
+class todo {
     constructor(title, description, due_date) {
         this.title = title;
         this.description = description;
@@ -7,36 +10,72 @@ class todos {
     }
 }
 
-// creates a project of which 0 or more 'todos' belong to
-class projects {
+//  a class representing a project
+class project {
     constructor(name) {
-        this.name = name
-        this.todos = []
+        this.name = name;
+        this.todos = [];
     }
 }
 
-const default_project = new projects('default');
-
-all_projects = [];
-all_projects.push(default_project);
-
-all_projects.forEach(project => {
-    var element = document.createElement('div')
-    element.textContent = project.name
-    document.body.appendChild(element)
-});
-
-// let newProjButton = document.getElementById('new-project-button')
-
-const project_form = document.getElementById('project-form');
-const project_input = document.getElementById('project-input');
-
-function showForm() {
+function showProjectForm() {
     project_form.style.visibility = 'visible';
+    project_form_input.value = '';
 }
 
-document.getElementById('submit-project').addEventListener("click", function(event) {
+document.getElementById('create-project-btn').addEventListener('click', (event) => {
     event.preventDefault();
     project_form.style.visibility = 'hidden';
-    console.log(project_input.value);
+    addProjectToLocal(new project(project_form_input.value));
 });
+
+
+//  returns the 'projects' item from local storage
+function getProjects() {
+    let projects = localStorage.getItem('projects');
+    return JSON.parse(projects);
+}
+
+/*
+  projects parameter: a list of project objects
+  set the list in local storage as 'projects'
+*/
+function setProjects(projects) {
+    projects = JSON.stringify(projects);
+    localStorage.setItem('projects', projects);
+}
+
+//  adds a new project to 'projects' in local storage
+function addProjectToLocal(project) {
+    projects = getProjects();
+    projects.push(project);
+    setProjects(projects);
+}
+
+/*
+  projects parameter: a list of project objects
+  creates a div with project name for each project in list
+*/
+function displayProjects(projects) {
+    projects.forEach(project => {
+        let div = document.createElement('div');
+        div.textContent = project.name;
+        document.body.appendChild(div);
+    });
+}
+
+//  flag to check if new or returning user
+const visited = localStorage.getItem('visited')
+if (visited) {
+    //  get projects from localStorage and display them
+    let projects = getProjects();
+    displayProjects(projects);
+} else {
+    //  mark that the user has now visited this site
+    localStorage.setItem('visited', 'true')
+
+    //  create the default project for first-time visitors
+    const default_project = new project('default');
+    let projects = [default_project];
+    setProjects(projects);
+}
